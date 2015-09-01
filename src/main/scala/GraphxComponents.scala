feb7b58530a323ed1f6332fcfb1a5f6b4cf0f328
Iulian Dogariu <code@iuliandogariu.com>
2015-09-01 14:28:50 +0300

    add a weighted shortest paths algorithm implemented using GraphX's Pregel-like API

--- src/main/scala/GraphxComponents.scala
+++ src/main/scala/GraphxComponents.scala
@@ -1,5 +1,6 @@
 import org.apache.spark._
 import org.apache.spark.graphx._
+import org.apache.spark.graphx.lib.ShortestPaths.SPMap
 import org.apache.spark.rdd.RDD
 
 object DeviceGraphTypes {
@@ -15,15 +16,17 @@ object DeviceGraphTypes {
   case object IOS     extends DeviceType
   case object Android extends DeviceType
 
-  case class GraphAdjacencyTag(source: EdgeSource, validAt: Long)
+  val emptySPMap: SPMap = Map.empty[VertexId, Int]
+  
+  case class Adjacency(source: EdgeSource, validAt: Long)
+
+  case class EdgeTag(adjacencies: Seq[Adjacency], distanceMap: SPMap)
+
+  case class VertexTag(id: String, devType: DeviceType)
 
   /* Syntactic sugar */
   def singleTag(source: EdgeSource, validAt: Long) =
-    Seq(GraphAdjacencyTag(source, validAt))
-
-  type EdgeTag = Seq[GraphAdjacencyTag]
-
-  case class VertexTag(id: String, devType: DeviceType)
+    EdgeTag(Seq(Adjacency(source, validAt)), emptySPMap)
 }
 
 import DeviceGraphTypes._
@@ -53,23 +56,86 @@ object GraphxComponents extends App {
   ))
 
   val graph = Graph(deviceInfo, deviceAdjacency)
+ // BEGIN connected components
   /*
-  println(graph.vertices.count)
-  println(graph.edges.count)
-  println(graph.vertices.first)
-
-  val facts: RDD[String] =
-    graph.triplets.map(triplet =>
-      triplet.srcAttr._1 + " is related in strength value as " + triplet.attr + " of " + triplet.dstAttr._1)
-  facts.collect.foreach(println(_))
-
-  println(graph.collectNeighborIds(EdgeDirection.Out))
-
-  println(graph.collectNeighbors(EdgeDirection.Out))
-  println(graph.vertices.collect().toList)
-*/
-
   val ccGraph = graph.connectedComponents()
 
   println(ccGraph.vertices.collect().toList)
+  */
+  // END connected components
+
+  val filteredIds : Seq[VertexId] = Seq(11, 12, 13, 18)
+
+
+  // BEGIN weighted Shortest paths
+  //
+  // See the unweighted version in the Spark GraphX library:
+  // https://github.com/apache/spark/blob/master/graphx/src/main/scala/org/apache/spark/graphx/lib/ShortestPaths.scala
+  //
+  // See the Pregel API docs at
+  // http://spark.apache.org/docs/latest/graphx-programming-guide.html#pregel-api
+
+  val OneWeek: Long = 7 * 24 * 60 * 60 * 1000L
+
+  val weights = graph.mapTriplets { edge =>
+    val adjacencies = edge.attr.adjacencies.
+      sortBy(- _.validAt) // Newest adjacencies are sorted first.
+
+    // Assign weight based on adjacencies
+    // Rules can be as complex as we want.
+    // The rules below are made up just to prove the point.
+
+    if( adjacencies.isEmpty )
+      Int.MaxValue // Absolute lowest weight if no adjacencies.
+    else if( adjacencies.exists(_.source == KO2O) )
+      // Highest weight if at least one adjacency from KO2O has ever existed.
+      1
+    else if( System.currentTimeMillis - adjacencies.head.validAt > OneWeek )
+      // Lower weight if the most recent adjacency is too old
+      4
+    else if( adjacencies.exists(_.source == JDPA) )
+      2
+    else if( adjacencies.forall(_.source == Tapad) )
+      // Not just one but all adjacencies come from Tapad
+      4
+    else
+      // Even lower weight in all other cases
+      16
+  }
+
+  def incrementMap(spmap: SPMap, distance: Int): SPMap
+    = spmap.map { case (v, d) => v -> (d + distance) }
+
+  def addMaps(spmap1: SPMap, spmap2: SPMap): SPMap =
+    (spmap1.keySet ++ spmap2.keySet).map {
+      k => k -> math.min(spmap1.getOrElse(k, Int.MaxValue), spmap2.getOrElse(k, Int.MaxValue))
+    }.toMap
+
+  val spGraph = weights.mapVertices { (vid, attr) =>
+    // when the filtered ids are joined to the weights we can
+    // check the attr instead of checking this sequence
+    if (filteredIds.contains(vid))
+      Map(vid -> 0)
+    else
+      emptySPMap
+  }
+
+  val initialMessage = emptySPMap
+
+  def vertexProgram(id: VertexId, attr: SPMap, msg: SPMap): SPMap = {
+    addMaps(attr, msg)
+  }
+
+  def sendMessage(edge: EdgeTriplet[SPMap, Int]): Iterator[(VertexId, SPMap)] = {
+    val dstAttr = incrementMap(edge.dstAttr, edge.attr)
+    val srcAttr = incrementMap(edge.srcAttr, edge.attr)
+
+    if (edge.srcAttr != addMaps(dstAttr, edge.srcAttr)) Iterator((edge.srcId, dstAttr))
+    else if (edge.dstAttr != addMaps(srcAttr, edge.dstAttr)) Iterator((edge.dstId, srcAttr))
+    else Iterator.empty
+  }
+
+  val distances = Pregel(spGraph, initialMessage, Int.MaxValue, EdgeDirection.Either)(vertexProgram, sendMessage, addMaps)
+
+  println(distances.vertices.take(20).mkString(" "))
 }
